15e1e6c663f1ab27da9343d984977efde5f97eae
Kamal Sharma <kamaldgrt@gmail.com>
2020-06-22 17:51:44 +0530

    make urls pretty

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -36,14 +36,15 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '/' => 'site/index'
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
