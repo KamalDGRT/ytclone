a8b029462744a5d7e9cd55995f5c212695f772ce
Kamal Sharma <kamaldgrt@gmail.com>
2020-06-23 23:58:46 +0530

    implement tags

--- backend/assets/TagsInputAsset.php
+++ backend/assets/TagsInputAsset.php
@@ -1,22 +1,9 @@
 <?php
-/**
- * User: TheCodeholic
- * Date: 4/17/2020
- * Time: 11:12 AM
- */
-
 namespace backend\assets;
-
 
 use yii\web\AssetBundle;
 use yii\web\JqueryAsset;
 
-/**
- * Class TagsInputAsset
- *
- * @author  Zura Sekhniashvili <zurasekhniashvili@gmail.com>
- * @package backend\assets
- */
 class TagsInputAsset extends AssetBundle
 {
     public $basePath = '@webroot/tagsinput';
